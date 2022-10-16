d66481676440f67f704819a0f1873710bae4780f
Austin Pereira <79310192+AUSt-in@users.noreply.github.com>
2022-10-16 02:10:36 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,9 +1,10 @@
-const selectBtn = document.querySelector(".select-btn"),
+const selectBtn = document.querySelectorAll(".select-btn"),
       items = document.querySelectorAll(".item");
 
-selectBtn.addEventListener("click", () => {
-    selectBtn.classList.toggle("open");
-});
+// selectBtn.addEventListener("click", () => {
+//     selectBtn.classList.toggle("open");
+// });
+
 
 items.forEach(item => {
     item.addEventListener("click", () => {
@@ -12,10 +13,11 @@ items.forEach(item => {
         let checked = document.querySelectorAll(".checked"),
             btnText = document.querySelector(".btn-text");
 
-            if(checked && checked.length > 0){
-                btnText.innerText = `${checked.length} Selected`;
-            }else{
-                btnText.innerText = "Select Language";
-            }
+            
+    });
+})
+selectBtn.forEach(item => {
+    item.addEventListener("click", () => {
+        item.classList.toggle("open");
     });
 })
